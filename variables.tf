49a5c79a00fb1f5e4d69f5316f263366406f2eab
bandaru5 <bbkrockit5@gmail.com>
2025-07-21 19:36:28 +0200

    Variables change

--- variables.tf
+++ variables.tf
@@ -25,3 +25,14 @@ variable "tenant_id" {
   description = "The Azure Tenant ID"  
   type        = string 
 }
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
