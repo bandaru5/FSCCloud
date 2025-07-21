496747d9425d9551a38f794ef52b9657e1af4368
bandaru5 <bbkrockit5@gmail.com>
2025-07-21 22:13:57 +0200

    changes in variables.tf

--- variables.tf
+++ variables.tf
@@ -60,3 +60,14 @@ variable "budget_contact_email" {
   type        = list(string)
   description = "List of email addresses for budget alerts"
 }
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
